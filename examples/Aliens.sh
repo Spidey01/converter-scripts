68b6ca118d376017beef1c5bfe1a92d72fc77c5a
Terry M. Poulin <BigBoss1964@gmail.com>
2013-12-15 00:32:45 +0000

    Mod the CRF for Aliens in examples/Aliens.sh.

--- examples/Aliens.sh
+++ examples/Aliens.sh
@@ -4,7 +4,15 @@
 INPUT_DIR=/srv/minidlna/content/Videos/Movies
 OUTPUT_DIR=/srv/minidlna/content/
 
-
+start="`date`"
+echo "Started on $start"
+x264_crf=26
+convert_simple_surround \
+    "$INPUT_DIR/Aliens (1986) Special Edition.mkv" \
+    "$OUTPUT_DIR/Aliens (1986) Special Edition.mp4"
+x264_crf=
+echo "Finished on `date`"
+exit
 #
 # these inputs are all 6 channel DTS.
 # make output downmixed 2 channel AAC-LC and 6-channel AAC-LC.
@@ -34,6 +42,14 @@ convert_simple_surround \
     "$INPUT_DIR/Alien - Resurrection (1997) Special Edition.mkv" \
     "$OUTPUT_DIR/Alien - Resurrection (1997) Special Edition.mp4"
 
+#
+# For these, the quality setting in convert_simple_surround generates way too
+# high (11~12 Mbit/s) video bitrates for the Chromecast. Do it manually'ish.
+#
+# By fiddling around I found that there's a big difference between 25 and 26
+# but the latter seems to keep the bitrate under the limit.
+#
+x264_crf=26
 convert_simple_surround \
     "$INPUT_DIR/Aliens (1986).mkv" \
     "$OUTPUT_DIR/Aliens (1986).mp4"
@@ -41,4 +57,5 @@ convert_simple_surround \
 convert_simple_surround \
     "$INPUT_DIR/Aliens (1986) Special Edition.mkv" \
     "$OUTPUT_DIR/Aliens (1986) Special Edition.mp4"
+x264_crf=
 
