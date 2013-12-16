4695009c26e2142e262677e16500f45186024a30
Terry M. Poulin <BigBoss1964@gmail.com>
2013-12-16 02:33:55 +0000

    Mod the quality of the Evil Dead examples.

--- examples/Evil-Dead.sh
+++ examples/Evil-Dead.sh
@@ -12,13 +12,17 @@ OUTPUT_DIR=/srv/minidlna/content/
 #   - 6-channel AAC-LC
 #   - 2 channel AAC-LC (commentary)
 #
+# Quality needs to be lowered however to keep it within the Chromecast's margins :'(.
+#
 
-
+x264_crf=26
 convert_simple_surround_with_commentary \
     "$INPUT_DIR/Army of Darkness (1992).mkv" \
     "$OUTPUT_DIR/Army of Darkness (1992).mp4"
 
+x264_crf=24
 convert_simple_surround_with_commentary \
     "$INPUT_DIR/Evil Dead II - Dead by Dawn (1987).mkv" \
     "$OUTPUT_DIR/Evil Dead II - Dead by Dawn (1987).mp4"
 
+x264_crf=
